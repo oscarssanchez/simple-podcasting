2fe12db079f75dcad57862fd440ce9c598482115
Oscar Sanchez <oscar.sanchez@10up.com>
2019-05-14 19:30:01 -0500

    Add transcript endpoint templates

--- includes/transcript-endpoint.php
+++ includes/transcript-endpoint.php
@@ -2,7 +2,7 @@
 /**
  * Transcript Endpoint
  *
- * @package tenup_podcasting\endpoints
+ * @package tenup_podcasting
  */
 
 namespace tenup_podcasting;
@@ -25,14 +25,21 @@ add_action( 'init', __NAMESPACE__ . '\add_transcript_endpoint' );
  *
  */
 function transcript_template( $template ) {
+	if ( ! empty( get_post_meta( get_the_ID(), 'podcast_transcript', true ) ) ) {
+		if ( get_query_var( 'transcript', false ) !== false ) {
+			$transcript_template = locate_template( array( 'template-transcript.php' ) );
+			if ( '' !== $transcript_template ) {
+				return $transcript_template;
+			}
 
-	if ( get_query_var( 'transcript', false ) !== false ) {
-		echo 'test';
+			$transcript_template = PODCASTING_PATH . 'templates/template-transcript.php';
+			if ( file_exists( $transcript_template ) ) {
+				return $transcript_template;
+			}
+		}
 	}
-
-	//Fall back to original template
+	//Fall back to default if there is no transcript
 	return $template;
-
 }
 
 add_action( 'template_include', __NAMESPACE__ . '\transcript_template' );
---
+++ templates/template-transcript.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+<div class="wrap">
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php
+					the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+					?>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<?php
+					$podcast_transcript = get_post_meta( get_the_ID(), 'podcast_transcript', true );
+					if ( ! empty( $podcast_transcript ) ) {
+						echo wp_kses_post( $podcast_transcript );
+					} else {
+						echo 'No transcript available';
+					}
+					?>
+				</div><!-- .entry-content -->
+
+			</article><!-- #post-${ID} -->
+				<?php
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+</div>
+<?php
+get_footer();
